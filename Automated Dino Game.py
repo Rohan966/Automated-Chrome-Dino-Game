e21da2aa70f413911d78c272ea99b9de5eca51a1
Abhinav Gupta <65679154+Abhinav250@users.noreply.github.com>
2020-10-01 18:07:22 +0530

    Added hit Function
    Now this code will work well. Added hit Function

--- Automated Dino Game.py
+++ Automated Dino Game.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageGrab # pip install pillow
 # from numpy import asarray
 import time
 
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
 def isCollide(data):
     # Draw the rectangle for birds
     for i in range(300, 415):
@@ -16,6 +20,7 @@ def isCollide(data):
             if data[i, j] < 100:
                 hit("up")
                 return
+    return
 
 if __name__ == "__main__":
     print("Hey.. Dino game about to start in 3 seconds")
@@ -42,4 +47,3 @@ if __name__ == "__main__":
         image.show()
         break
       '''
-
